37b2c87415fdf525e0613ec1d41972e370117a21
Boris Ts. (cborka) <cborka777@yandex.ru>
2018-07-16 17:06:15 +0700

    Загрузка из файла, 1 вариант.

--- routes/pro.js
+++ routes/pro.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 var db = require("../db");
 
@@ -252,6 +253,57 @@ router.get('/fc_delete/:fc_id', function(req, res, next) {
         res.send(error);
       });
   });
+
+//
+// Загрузить список ЖБИ из файла с разделителями
+//
+router.get('/fc_load', function(req, res, next) {
+
+  file_name = 'c:\\cborka\\fcl.txt';
+
+
+// Считываем содержание файла в память
+  fs.readFile(file_name, function (err, logData) {
+
+    // Если возникла ошибка, мы кидаем исключение
+    // и программа заканчивается
+    if (err) throw err;
+
+    // logData это объект типа Buffer, переводим в строку
+    var text = logData.toString();
+
+
+    var ret = '';
+
+    // Разбиваем текст на массив из строчек
+    var lines = text.split('\n');
+
+    var arr = ["Яблоко", "Апельсин", "Груша"];
+
+    for (var i = 0; i < lines.length; i++) {
+      var parts = lines[i].split('|');
+      ret = ret + ', nm='+parts[0]+', v='+parts[1];
+
+      db.none(
+        "INSERT INTO fc_list (fc_name, fc_v, bet_v, fc_w, concrete_rf, ok, notes) " +
+        "VALUES ($1, $2, 0, 0, 1, '', '')",
+        [parts[0],parts[1]])
+        .then (function (data) {
+          //res.redirect('/pro/fcs');
+
+        })
+        .catch(function (error) {
+          res.send(error);
+        });
+
+    }
+    res.send(ret);
+  });
+
+});
+
+
+
 
 //=================== ФОРМЫ для формовки ЖБИ =====================
 
